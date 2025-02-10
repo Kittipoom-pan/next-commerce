0a9164e883e19dc8393168885612927314cabc4a
kittipoom.p <kittipoom.panyan@gmail.com>
2025-02-10 17:37:02 +0700

    add product list page

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,5 +1,32 @@
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
 const ListPage = () => {
-  return <div>ListPage</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="bg-pink-50 p-4 flex justify-between h-64">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+            Grab up to 50 off on
+            <br /> Selected product
+          </h1>
+          <button className="rounded-3xl bg-red w-max py-3 px-5 text-sm text-white">
+            Buy Now
+          </button>
+        </div>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt={""} fill className="object-contain" />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <ProductList />
+    </div>
+  );
 };
 
 export default ListPage;
